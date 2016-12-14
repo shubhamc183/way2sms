4ac174db18baa5ae9bd6244bcc586bfa05cf7d23
Shubham Choudhary <shubhamc183@gmail.com>
2016-12-14 20:27:38 +0530

    Done formatting

---
+++ way2sms.py
@@ -0,0 +1,97 @@
+import requests
+from bs4 import BeautifulSoup
+
+class sms:
+
+	def __init__(self,username,password):
+
+		'''
+		Takes username and password as parameters for constructors
+		and try to log in
+		'''
+
+		self.url='http://site24.way2sms.com/Login1.action?'
+
+		self.cred={'username': username, 'password': password}
+
+		self.s=requests.Session()			# Session because we want to maintain the cookies
+
+		'''
+		changing s.headers['User-Agent'] to spoof that python is requesting
+		'''	
+	
+		self.s.headers['User-Agent']="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"
+
+		self.q=self.s.post(self.url,data=self.cred)
+
+		self.loggedIn=False				# a variable of knowing whether logged in or not
+
+		if self.q.status_code!=200:			# http status 200 == OK
+
+			self.loggedIn=False
+
+		else:
+
+			self.loggedIn=True
+
+		self.jsid=self.s.cookies.get_dict()['JSESSIONID'][4:]	    # JSID is the main KEY as JSID are produced every time a session satrts
+
+	def msgSentToday(self):
+		
+		'''
+		Returns number of SMS sent today as there is a limit of 100 messages everyday..!
+		'''
+
+		self.msg_left_url='http://site24.way2sms.com/sentSMS?Token='+self.jsid
+
+		self.q=self.s.get(self.msg_left_url)
+
+		self.soup=BeautifulSoup(self.q.text,'html.parser')		#we want the number of messages sent which is present in the 	
+
+		self.t=self.soup.find("div",{"class":"hed"}).h2.text		# div element with class "hed" -> h2 
+
+		self.sent=0
+
+		for self.i in self.t:
+
+			if self.i.isdecimal():
+
+				self.sent=10*self.sent+int(self.i)
+
+		return self.sent
+		
+	def send(self,mobile_no,msg):
+		
+		'''
+		Sends the message to the given mobile number 
+		'''
+	
+		if len(msg)>139 or len(mobile_no)!=10 or not mobile_no.isdecimal():	#checks whether the given message is of length more than 139
+
+			return False							#or the mobile_no is valid
+
+		self.payload={'ssaction':'ss',
+				'Token':self.jsid,					#inorder to visualize how I came to these payload,
+			        'mobile':mobile_no,					#must see the NETWORK section in Inspect Element 
+       				 'message':msg,						#while messagin someone from your browser
+			        'msgLen':'129'
+       			     }
+
+		self.msg_url='http://site24.way2sms.com/smstoss.action'
+
+		self.q=self.s.post(self.msg_url,data=self.payload)
+
+		if self.q.status_code==200:
+
+			return True
+
+		else:
+			return False
+
+	def logout(self):
+
+		self.s.get('http://site24.way2sms.com/entry?ec=0080&id=dwks')
+
+		self.s.close()								# close the Session
+
+		self.loggedIn=False
